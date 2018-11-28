eeb1d4c8288a62e178167a5947a5334c9781d0b3
Taha <>
2018-11-28 10:40:03 +0200

    update HeaderCardView

--- VUIComponents/Classes/Components/Card/Subclass/HeaderCardView.h
+++ VUIComponents/Classes/Components/Card/Subclass/HeaderCardView.h
@@ -8,15 +8,26 @@
 
 #import "BaseCardView.h"
 
+typedef enum : int {
+    Title=0,
+    SubTitle=1,
+    AvatarImg=2,
+    Desc=3
+} CustomHeight;
 IB_DESIGNABLE
 @interface HeaderCardView : BaseCardView
 
 @property (nonatomic , strong) IBInspectable NSString * titleString;
 @property (nonatomic , strong) NSAttributedString * subTitleAttributedString;
 @property (nonatomic , strong) NSAttributedString * descAttributedString;
+@property (nonatomic , strong) NSAttributedString * footerAttributedString;
+
+@property (nonatomic , strong) UIImage * avatarImg;
 
 @property (nonatomic , strong) NSArray *buttons;
 
 @property (nonatomic ) IBInspectable int titleTopConstraintConstant;
+-(float)getHeightTo:(CustomHeight)customHeight; // to use in StretchHeaderView
 
 @end
+
--- VUIComponents/Classes/Components/Card/Subclass/HeaderCardView.m
+++ VUIComponents/Classes/Components/Card/Subclass/HeaderCardView.m
@@ -14,18 +14,31 @@
 @interface HeaderCardView(){
     
 }
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *buttonsViewHeighConstraint;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *buttonsViewBottomConstraint;
 @property (weak, nonatomic) IBOutlet UIView *buttonViewContainer;
+@property (weak, nonatomic) IBOutlet UIImageView *avatarImgView;
 @property (strong, nonatomic) IBOutletCollection(UIView) NSArray *filterViews;
 @property (weak, nonatomic) IBOutlet AnaVodafoneLabel *titleLabel;
 @property (weak, nonatomic) IBOutlet AnaVodafoneLabel *subtitleLabel;
 @property (weak, nonatomic) IBOutlet AnaVodafoneLabel *descLabel;
+@property (weak, nonatomic) IBOutlet AnaVodafoneLabel *footerLabel;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *subttileLabelTopConstraint;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *descLabelTopConstraint;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *descLabelBottomConstraint;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *titleLabelTopConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *avatarImageTopConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *avatarImageHeightConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *buttonsViewHeighConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *buttonsViewBottomConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *footerLabelBottomConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *footerHeightConstraint;
+
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *filterSeparator;
+@property (weak, nonatomic) IBOutlet UIView *containerView;
+@property (nonatomic) float heightToTitle;
+@property (nonatomic) float heightToSubTitle;
+@property (nonatomic) float heightToDesc;
+@property (nonatomic) float heightToAvaterImg;
+@property (nonatomic) BOOL setupFilterViewFlag;
 
 @end
 @implementation HeaderCardView
@@ -40,7 +53,7 @@
     
     [self layoutIfNeeded];
     [self initialize];
-
+    
 }
 
 -(void)setSubTitleAttributedString:(NSAttributedString *)subTitleAttributedString{
@@ -54,14 +67,19 @@
         _descLabelTopConstraint.constant = 0;
     }else{
         
-    _descLabelTopConstraint.constant = 15;
+        _descLabelTopConstraint.constant = 15;
     }
     _subtitleLabel.attributedText = subTitleAttributedString;
     
-    for (UIView * v in _filterViews) {
-        v.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.69];
-    }
+    [self setupFilterView];
+    
     [self layoutIfNeeded];
+    
+    if (_avatarImg) {
+        
+        [self setAvatarImg:_avatarImg];
+    }
+    
     [self initialize];
 }
 
@@ -70,26 +88,67 @@
     _descAttributedString = descAttributedString;
     _descLabelBottomConstraint.constant = 15;
     _descLabelTopConstraint.constant = 15;
-
+    
     _descLabel.attributedText = descAttributedString;
-    for (UIView * v in _filterViews) {
-        v.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.69];
-    }
     
     self.filterSeparator.constant = 1;
+    
+    [self setupFilterView];
     [self layoutIfNeeded];
     [self initialize];
 }
 
+-(void)setFooterAttributedString:(NSAttributedString *)footerAttributedString{
+    
+    _footerAttributedString = footerAttributedString;
+    
+    _footerLabelBottomConstraint.constant = 20;
+    _buttonsViewBottomConstraint.constant = 0;
+    _footerLabel.attributedText = footerAttributedString;
+    
+    [self setupFilterView];
+    [self layoutIfNeeded];
+    [self initialize];
+}
+
+-(void)setAvatarImg:(UIImage *)avatarImg{
+    
+    _avatarImg = avatarImg;
+    
+    _avatarImgView.image = _avatarImg;
+    
+    _avatarImgView.layer.cornerRadius = 40;
+    
+    _avatarImgView.clipsToBounds = true;
+    
+    _avatarImageTopConstraint.constant = 30;
+    
+    _avatarImageHeightConstraint.constant = 80;
+    
+    _subttileLabelTopConstraint.constant += 110;
+    
+    [self initialize];
+    
+}
 - (void)setButtons:(NSArray *)buttons{
     
     _buttons = buttons;
     
     if (_buttons.count > 0) {
-        _buttonsViewBottomConstraint.constant = 10;
+        _buttonsViewBottomConstraint.constant = ([_footerAttributedString length] > 0)? 0 : 10;
         [self initializeButtonsView];
         [self layoutIfNeeded];
+    }
+}
 
+-(void)setupFilterView{
+    
+    if (_setupFilterViewFlag) {
+        return;
+    }
+    _setupFilterViewFlag = true;
+    for (UIView * v in _filterViews) {
+        v.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.69];
     }
 }
 
@@ -120,12 +179,12 @@
 -(void)setTitleTopConstraintConstant:(int)titleTopConstraintConstant{
     
     _titleTopConstraintConstant = titleTopConstraintConstant;
-
+    
     _titleLabelTopConstraint.constant = titleTopConstraintConstant;
-
+    
     [self layoutIfNeeded];
     [self initialize];
-
+    
 }
 
 -(void)layoutSubviews{
@@ -134,26 +193,68 @@
 #pragma mark height adjustment
 
 -(void)initializeContentView{
-
-    contentViewHeight = 77;
-
+    
+    contentViewHeight = 27+14;
+    
     if ([_titleString length] > 0){
-
+        
         [_titleLabel adjustHeight];
         contentViewHeight += _titleLabel.frame.size.height;
+        _heightToTitle = _containerView.frame.origin.y;
     }
     if ([_subTitleAttributedString length] > 0){
-
+        
         [_subtitleLabel adjustHeight];
         contentViewHeight += _subtitleLabel.frame.size.height;
+        _heightToSubTitle = _heightToTitle + _subtitleLabel.frame.size.height + _subttileLabelTopConstraint.constant + _descLabelTopConstraint.constant;
     }
     if ([_descAttributedString length] > 0){
-
+        
         [_descLabel adjustHeight];
         contentViewHeight += _descLabel.frame.size.height;
+        _heightToDesc = _heightToTitle + _subtitleLabel.frame.size.height + _subttileLabelTopConstraint.constant + _descLabelTopConstraint.constant + _descLabelBottomConstraint.constant + _descLabel.frame.size.height;
     }
-    contentViewHeight += _subttileLabelTopConstraint.constant + _descLabelTopConstraint.constant + _descLabelBottomConstraint.constant + _titleLabelTopConstraint.constant +_buttonsViewHeighConstraint.constant + _buttonsViewBottomConstraint.constant;
+    
+    if ([_footerAttributedString length] > 0) {
+        
+        [_footerLabel adjustHeight];
+        
+        _footerHeightConstraint.constant = _footerLabel.frame.size.height;
+        
+        contentViewHeight += _footerHeightConstraint.constant;
+        
+    }
+    
+    _heightToAvaterImg = (_avatarImg)? _heightToTitle +_subttileLabelTopConstraint.constant:_heightToTitle;
+    
+    contentViewHeight += _titleLabelTopConstraint.constant + _subttileLabelTopConstraint.constant + _descLabelTopConstraint.constant + _descLabelBottomConstraint.constant + _buttonsViewHeighConstraint.constant + _buttonsViewBottomConstraint.constant + _footerLabelBottomConstraint.constant;
+    
 }
+
+// to use in StretchHeaderView
+-(float)getHeightTo:(CustomHeight)customHeight{
+    
+    switch (customHeight) {
+        case Title:
+            return _heightToTitle;
+            break;
+        case SubTitle:
+            return _heightToSubTitle;
+            break;
+        case AvatarImg:
+            return _heightToAvaterImg;
+            break;
+        case Desc:
+            return _heightToDesc;
+            break;
+            
+        default:
+            break;
+    }
+    
+    return 0;
+}
+
 -(void)commonInit{
     
     [super commonInit];
@@ -167,7 +268,8 @@
     [self.subviews makeObjectsPerformSelector: @selector(removeFromSuperview)];
     self.filterSeparator.constant = 0 ;
     _buttonsViewBottomConstraint.constant = 16;
-
+    
     [self addSubview:view];
 }
 @end
+
